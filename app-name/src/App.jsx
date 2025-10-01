34a725c0c94767eed60aecb836b43657b0350bfa
ryousuf569569 <ryousuf569@gmail.com>
2025-10-01 12:09:00 -0400

    made some extra additions before class

--- app-name/src/App.jsx
+++ app-name/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [contacts, setContacts] = useState([])
 
-  co
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.01:5000/contacts")
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
 
   return (
     <>
